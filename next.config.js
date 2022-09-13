65d7889a60c39f2777a2aa1320bec08223b619f4
PedroL <pedrolucassantosferreira426@gmail.com>
2022-09-13 20:08:07 -0300

    novo: env.local.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    swcMinify: true
+    swcMinify: true,
+    env: {
+        API_GEO_LOCATION_TOKEN: process.env.API_GEO_LOCATION_TOKEN,
+        API_GEO_LOCATION_URL: process.env.API_GEO_LOCATION_URL,
+        API_WEATHER_URL: process.env.API_WEATHER_URL,
+        API_WEATHER_TOKEN: process.env.API_WEATHER_TOKEN
+    }
 };
 
 module.exports = nextConfig;
